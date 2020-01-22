e02eac673f43ca830f8e8062e6c9232787373869
Camshaft54 <cam.shaw1234@gmail.com>
2020-01-21 20:55:13 -0800

    Fix Typo Add fields for custom operands

--- Affine Cipher.py
+++ Affine Cipher.py
@@ -1,14 +1,21 @@
 # v1.0
 # Created by CamShaft54, Reevak05, and MetalTurtle18
 
+import random
+
 import PySimpleGUI as Sg
 
 # uncomment the lines below when we add additional ciphers
-setup_layout = [  # [Sg.Text('Welcome to Affine Cipher')],
+setup_layout = [
+    # [Sg.Text('Welcome to Affine Cipher')],
     # [Sg.Text('Select your Cipher Type')],
     # [Sg.Radio('Affine Cipher', "cipher_type", default=True)],
     [Sg.Radio('Encrypt      ', "method", default=True, key='encrypt_input'),
      Sg.Radio('Decrypt', "method", key='decrypt_input')],
+    [Sg.Text('Enter operands for encryption/decryption:')],
+    [Sg.Text('Multiplier or Divisor:'), Sg.Input(default_text='1', key='multiplier'),
+     Sg.Text('Addend or Minuend:'), Sg.Input(default_text='0', key='addend'),
+     Sg.Checkbox('Random', default=False, key='random')],
     [Sg.Text('Input:')],
     [Sg.Multiline(size=(70, 5), enter_submits=True, key='code_input', do_not_clear=False)],
     [Sg.Text('Output:')],
@@ -27,14 +34,14 @@ def char_to_num(k):
 
 # This function multiplies a number then adds a number and returns mod 26 of that
 def mod():
-    x = ((int(number_input) * 9) + 7) % 26
+    x = ((int(number_input) * multiplier) + addend) % 26
     return x
 
 
 # This function is a modified modular multiplicative inverse
 def modInverse(y):
     for k in range(0, 26):
-        if (9 * k) % 26 == ((y - 7) % 26):
+        if (9 * multiplier) % 26 == ((y - addend) % 26):
             return k
     return k
 
@@ -51,6 +58,14 @@ while True:
     if event in (None, 'EXIT'):
         break
     if event == 'Submit':
+        multiplier = int(values['multiplier'])
+        addend = int(values['addend'])
+        random_operand = values['random']
+        if random_operand:
+            multiplier = random.randint(2, 10)
+            addend = random.randint(2, 10)
+        print("Multiplier/Divisor = " + str(multiplier))
+        print("Addend/Minuend = " + str(addend))
         code_input = str(values['code_input'])
         char_list = list(code_input)
         if values['encrypt_input']:  # Driver code for encryption
